b31d5fd92b0544b8d3db343b29420e0b359671a8
JK90MO <gabriel.zeus.reyes@ing.com>
2026-02-05 15:17:04 +0800

    feat: v1.1

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    allowedHosts: ["1gudfu-ip-136-158-78-205.tunnelmole.net"],
+    allowedHosts: ["6wlleu-ip-210-57-14-5.tunnelmole.net"],
   },
   plugins: [react()],
 });
